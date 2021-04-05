73bb2392d2a94750d13287d581393bffd85bad29
Olivier Drevet <odrevet@gmail.com>
2021-04-05 09:01:47 +0200

    init NPC and chest in the event exec warp function

--- demo/RPG/src/levels/levels.c
+++ demo/RPG/src/levels/levels.c
@@ -4,19 +4,6 @@ void level_overworld(game *p_game, SDL_Renderer *renderer)
 {
   level_map_init_from_file(&p_game->o_level, "data/overworld.map", renderer);
   level_init_from_file(&p_game->o_level, "data/overworld.level", &p_game->path_music, renderer);
-
-  for (int chest_index = 0; chest_index < p_game->o_level.chest_count; chest_index++)
-  {
-    chest_init(p_game->o_level.p_chest + chest_index, p_game->p_tilesets + 1, renderer);
-    p_game->o_level.p_chest[chest_index].is_open = game_get_chest_is_open(p_game, p_game->o_level.p_chest[chest_index].id);
-    SDL_Rect bounding_box = {.x = p_game->o_level.p_chest[chest_index].o_sprite.x, .y = p_game->o_level.p_chest[chest_index].o_sprite.y, .w = 16, .h = 16};
-    p_game->o_level.p_chest[chest_index].o_sprite.bounding_box = bounding_box;
-  }
-
-  for (int NPC_index = 0; NPC_index < p_game->o_level.NPC_count; NPC_index++)
-  {
-    NPC_init(p_game->o_level.p_NPC + NPC_index, p_game->o_level.p_tileset + 0, renderer);
-  }
 }
 
 void level_cave(game *p_game, SDL_Renderer *renderer)
